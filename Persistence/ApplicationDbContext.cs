253bbebb2d2e572fc07709fa5178c983a2a66442
Diego Mogollon <diego.mogollon.camacho@gmail.com>
2024-08-28 14:46:53 -0500

    fix: correct the entities so that they do not intersect with the reserved words of the language.

--- Persistence/ApplicationDbContext.cs
+++ Persistence/ApplicationDbContext.cs
@@ -14,22 +14,22 @@ namespace Persistence
         {
         }
 
-        public virtual DbSet<Domain.Models.Attribute> Attributes { get; set; }
-        public virtual DbSet<Attribute_Value> Attribute_Value { get; set; }
+        public virtual DbSet<Characteristics> Characteristics { get; set; }
+        public virtual DbSet<Characteristic_Value> Characteristic_Value { get; set; }
         public virtual DbSet<Category> Category { get; set; }
-        public virtual DbSet<Category_Attribute> Category_Attribute { get; set; }
+        public virtual DbSet<Category_Characteristic> Category_Characteristic { get; set; }
         public virtual DbSet<Product> Products { get; set; }
         public virtual DbSet<Product_Category> Product_Category { get; set; }
         public virtual DbSet<Product_Subcategory> Product_Subcategory { get; set; }
         public virtual DbSet<Subcategory> Subcategory { get; set; }
-        public virtual DbSet<Subcategory_Attribute> Subcategory_Attribute { get; set; }
+        public virtual DbSet<Subcategory_Characteristic> Subcategory_Characteristic { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Product_Category>().HasKey(pc => new { pc.Id_Category, pc.Id_Product });
             modelBuilder.Entity<Product_Subcategory>().HasKey(ps => new { ps.Id_Subcategory, ps.Id_Product });
-            modelBuilder.Entity<Category_Attribute>().HasKey(ca => new {ca.Id_Category, ca.Id_Attribute });
-            modelBuilder.Entity<Subcategory_Attribute>().HasKey(sa => new {sa.Id_Subcategory, sa.Id_Attribute});
+            modelBuilder.Entity<Category_Characteristic>().HasKey(ca => new {ca.Id_Category, ca.Id_Characteristic });
+            modelBuilder.Entity<Subcategory_Characteristic>().HasKey(sa => new {sa.Id_Subcategory, sa.Id_Characteristic });
         }
     }
 }
