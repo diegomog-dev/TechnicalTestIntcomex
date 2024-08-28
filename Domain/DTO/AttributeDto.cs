e69ca02fcfcc087310416a4e0a3e26cd2d41b927
Diego Mogollon <diego.mogollon.camacho@gmail.com>
2024-08-27 21:09:29 -0500

    feat: Add DTO entities.

---
+++ Domain/DTO/AttributeDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTO
+{
+    public class AttributeDto
+    {
+        public string Attribute_Name { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ Domain/DTO/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTO
+{
+    public class ProductDto
+    {
+        public int sku { get; set; }
+        public string mpn { get; set; }
+        public string product_name { get; set; }
+        public List<AttributeDto> AttributesList { get; set; }
+    }
+}
