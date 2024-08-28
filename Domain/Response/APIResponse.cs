d2d15fe657ab32a2b35e63be0559de714429455d
Diego Mogollon <diego.mogollon.camacho@gmail.com>
2024-08-27 21:06:54 -0500

    feat: Add entity to send a parameterized response.

--- Domain/Response/APIResponse.cs
+++ Domain/Response/APIResponse.cs
@@ -14,7 +14,7 @@ namespace Domain.Response
             ErrorMessages = new List<string>();
         }
         public HttpStatusCode StatusCode { get; set; }
-        public bool IsSuccess { get; set; }
+        public bool IsSuccess { get; set; } = true;
         public List<string> ErrorMessages { get; set;}
         public object Result { get; set; }
         public int TotalPages { get; set; }
