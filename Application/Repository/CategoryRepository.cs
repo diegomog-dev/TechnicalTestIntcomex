9f8e6506e34515669b30d4fbb05560be70e66d62
Diego Mogollon <diego.mogollon.camacho@gmail.com>
2024-08-30 11:21:23 -0500

    feat: add CategoryRepository and SubcategoryRepository

---
+++ Application/Repository/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.Repository.IRepository;
+using Domain.Models;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryRepository(ApplicationDbContext context) :base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ Application/Repository/SubcategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.Repository.IRepository;
+using Domain.Models;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repository
+{
+    public class SubcategoryRepository : Repository<Subcategory>, ISubcategoryRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SubcategoryRepository(ApplicationDbContext context) :base(context)
+        {
+            _context = context;
+        }
+    }
+}
